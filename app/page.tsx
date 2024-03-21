03b827ecffc52fbca734cfb14b7d081c31453bee
Pulok <pulok@property.credit>
2024-03-21 18:47:59 +1100

    horse style fix

--- app/page.tsx
+++ app/page.tsx
@@ -68,16 +68,16 @@ export default function Home() {
           My Projects
         </Link>
       </div>
-      <div className="absolute bottom-0 right-0 z-[10]">
+      <div className="absolute bottom-0 right-0 z-[10] hidden lg:block ">
         <Image
           src="/horse.png"
           alt="horse"
           height={300}
-          width={300}
+          width={220}
           className="absolute right-55 top-40"
         />
 
-        <Image src="/cliff.webp" alt="cliff" width={480} height={480} />
+        <Image src="/cliff.webp" alt="cliff" width={400} height={480} />
       </div>
       <div className="absolute bottom-0 z-[5] w-full h-full">
         <Image
